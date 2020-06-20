556ae907bb945f6a08bcacabcf45eb8f4a8ae069
otumian <popecan1000@gmail.com>
2020-06-20 17:57:25 +0000

    completed implementation, python - eqrat fail

--- rational_num_sys.scm
+++ rational_num_sys.scm
@@ -16,10 +16,51 @@ this system must be able to add, subtract, multipy, divide and check the equalit
     (define g (gcd n d))
     (cons (/ n g) (/ d g)))
 
-; add rational numbers
-(define (addrat x y)
-    (let ((n (+ ( * (numrat x)
-                 (denumrat y))
+; patterned procedure
+(define (pattern-proc proc x y)
+    (let ((n (proc 
+                ( * (numrat x) (denumrat y))
                 (* (denumrat x) (numrat y))))
             (d (* (denumrat x) (denumrat ))))
+
         (makerat n d)))
+
+; add rational numbers
+(define (addrat x y)
+    (pattern-proc + x y))
+
+; subtracts rational numbers
+(define (subrat x y)
+    (pattern-proc - x y))
+
+; multiplies rational numbers
+(define (mulrat x y)
+    (makerat (* (numrat x)
+                (numrat y))
+            (* (denumrat x)
+                (denumrat y))))
+
+; divides rational numbers
+(define (divrat x y)
+    (makerat (* (numrat x)
+                (denumrat y))
+            (* (denumrat x)
+                (numrat y))))
+
+; check for equality of rational numbers
+(define (eqrat x y)
+    (= (* (numrat x) (denumrat y))
+        (* (denumrat x) (numrat y))))
+
+; prints a given rational number
+(define (printrat r)
+    (newline)
+    (display (numrat r))
+    (display "/")
+    (display (denumrat r))
+    nil)  ; let it return nil
+
+
+; (load "rational_num_sys.scm")
+(define half (makerat 1 2))
+(define third (makerat 1 3))
